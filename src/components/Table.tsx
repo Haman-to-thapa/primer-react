bb281a066ed4ce76c3d9d9cf9ff6a581cb23bfaf
Haman-to-thapa <hamanthapa0000@gmail.com>
2026-01-11 11:22:44 +0530

    add prime-raeact table add in table.jsx setup

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,26 +1,43 @@
 import { useEffect, useState } from "react";
 import { fetchApi } from "../api/api";
 
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+
 function Table() {
+  const [dataFetching, setDataFetching] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const loadData = async () => {
       try {
+        setLoading(true);
         const response = await fetchApi();
-        console.log("Api response", response)
-
+        setDataFetching(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
       }
-    }
-    loadData()
-  }, [])
+    };
+
+    loadData();
+  }, []);
 
   return (
-    <div>
-
-    </div>
-  )
+    <DataTable
+      value={dataFetching}
+      loading={loading}
+      dataKey="id"
+    >
+      <Column field="title" header="Title" />
+      <Column field="place_of_origin" header="Origin" />
+      <Column field="artist_display" header="Artist" />
+      <Column field="inscriptions" header="Inscriptions" />
+      <Column field="date_start" header="Start Year" />
+      <Column field="date_end" header="End Year" />
+    </DataTable>
+  );
 }
 
-export default Table
+export default Table;
