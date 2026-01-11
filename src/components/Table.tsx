1b02a51935f2860fe4180c8dcc289e1f89936d0d
Haman-to-thapa <hamanthapa0000@gmail.com>
2026-01-11 12:05:13 +0530

    checkbox ui setup completed

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -3,18 +3,21 @@ import { fetchApi } from "../api/api";
 
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import { Checkbox } from 'primereact/checkbox';
+
 
 
 function Table() {
   const [dataFetching, setDataFetching] = useState<any[]>([]);
   const [loading, setLoading] = useState(false);
 
+
+
   useEffect(() => {
     const loadData = async () => {
       try {
         setLoading(true);
         const response = await fetchApi();
+        console.log(response)
         setDataFetching(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -26,19 +29,34 @@ function Table() {
     loadData();
   }, []);
 
+
+
   return (
-    <DataTable
-      value={dataFetching}
-      loading={loading}
-      dataKey="id"
-    >
-      <Column field="title" header="Title" />
-      <Column field="place_of_origin" header="Origin" />
-      <Column field="artist_display" header="Artist" />
-      <Column field="inscriptions" header="Inscriptions" />
-      <Column field="date_start" header="Start Year" />
-      <Column field="date_end" header="End Year" />
-    </DataTable>
+    <div className="px-5">
+
+      <DataTable
+        value={dataFetching}
+        loading={loading}
+        dataKey="id"
+
+      >
+        <Column
+          selectionMode="multiple"
+          headerStyle={{ width: "3rem" }}
+          bodyStyle={{ paddingLeft: "0rem", textAlign: "center" }}
+
+        />
+
+        <Column field="title" header="Title" />
+        <Column field="place_of_origin" header="Origin" />
+        <Column field="artist_display" header="Artist" />
+        <Column field="inscriptions" header="Inscriptions" />
+        <Column field="date_start" header="Start Year" />
+        <Column field="date_end" header="End Year" />
+      </DataTable>
+
+    </div>
+
   );
 }
 
